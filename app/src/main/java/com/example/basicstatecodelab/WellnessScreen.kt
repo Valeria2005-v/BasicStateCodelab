765a99245e264f5e91fa7ee3ab7cafccba7f0221
Valeria Salinas <s23120253@alumnos.itsur.edu.mx>
2026-09-23 20:26:12 -0600

    .

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,9 +1,2 @@
 package com.example.basicstatecodelab
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
-}
